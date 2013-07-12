02d0ab7dc5386b8efb98c32da004a88603589535
kishore <kishorerr@yahoo.com>
2013-07-11 17:41:59 -0700

    Updated grader

--- grader.js
+++ grader.js
@@ -7,8 +7,10 @@ references
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://shielded-wave-9616.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -18,6 +20,12 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var assertUrlExists = function(url) {
+    rest.get(url).on('complete', function(res) {
+	return res;
+    });
+};	    
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -46,6 +54,7 @@ if(require.main == module) {
     program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url_address>','Path to url address', clone(assertUrlExists), URL_DEFAULT)
     .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
